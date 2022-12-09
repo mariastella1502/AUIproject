d171bffbb4dce11c812bb56866ee3c14c98ff95f
Francesco <dttfrancesco@gmail.com>
2022-12-09 20:25:58 +0100

    Stoppati anche gli asteroidi diretti al target di distrazione

--- Assets/Uranus/Scripts/MoveToFake.cs
+++ Assets/Uranus/Scripts/MoveToFake.cs
@@ -7,18 +7,20 @@ public class MoveToFake : MonoBehaviour
     private Vector3 urPos;
     public float speed = 5;
     public float rotSpeed = 5;
+    UranusLife uranusLife;
 
     private void Start()
     {
         if (GameObject.Find("NoiseTarget") != null)
         {
             urPos = GameObject.Find("NoiseTarget").transform.position;
+            uranusLife = GameObject.Find("Uranus").GetComponent<UranusLife>();
         }
     }
 
     void Update()
     {
-        if (GameObject.Find("NoiseTarget") != null)
+        if (GameObject.Find("NoiseTarget") != null && uranusLife.isPause == false)
         {
             transform.position = Vector3.MoveTowards(transform.position, urPos, Time.deltaTime * speed);
             transform.RotateAround(transform.position, transform.up, rotSpeed * Time.deltaTime);
