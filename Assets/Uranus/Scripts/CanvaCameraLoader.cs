24594d59080eafd55cd61f05b4821930ac7797f7
Francesco <dttfrancesco@gmail.com>
2022-12-10 18:58:24 +0100

    Fixato overlay dei label sulla mano al ricaricamento della scena

---
+++ Assets/Uranus/Scripts/CanvaCameraLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NRKernal;
+
+
+public class CanvaCameraLoader : MonoBehaviour
+{
+    Canvas canvas;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        canvas = gameObject.AddComponent<Canvas>();
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = Camera.main;
+        canvas.planeDistance = 1;
+        gameObject.AddComponent<CanvasRaycastTarget>();
+        
+    }
+
+    
+}
